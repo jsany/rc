025948732d3e099f6bc1efc746f0c31b674442ef
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2020-10-16 17:27:33 +0800

    fix: 🐛 deploy gh add exportStatic aviod refresh 404

--- .umirc.ts
+++ .umirc.ts
@@ -15,5 +15,6 @@ const config: IConfig = {
   // more config: https://d.umijs.org/config
   publicPath: '/rc/',
   base: '/rc/',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
 };
 export default defineConfig(config);
